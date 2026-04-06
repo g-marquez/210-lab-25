700e84e4ebcb1134630472d44a17e4369915b15d
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-04-05 17:03:29 -0700

    update

--- main.cpp
+++ main.cpp
@@ -7,10 +7,29 @@
 
 #include <iostream>
 #include <chrono>
+#include <vector>
+#include <list>
+#include <set>
+#include <algorithm> //for sorting vector
+
 using namespace std;
 
 int main() {
-    
+    //create empty containers
+    vector<string> vector;
+    list<string> list;
+    set<string> set;
+
+    ifstream fin("codes.txt");
+    if (fin.good( )) {
+        
+        }
+        fin.close( );
+    }
+    else {
+        cout << "ERROR! Please verify file name/directory and restart program.";
+        return 1;
+    }
 
     return 0;
 }
